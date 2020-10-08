26860c00474970bdd49eb90c7277a0bd1728642c
Zacharias Knudsen <zachasme@gmail.com>
2020-10-08 11:42:41 +0200

    wip: Windows CMake support

--- .tmp-tools/entrypoint.sh
+++ .tmp-tools/entrypoint.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
 set -e
 
+sudo -u postgres mkdir build
+
 service postgresql start
 
-# first regular cmake
-sudo -u postgres cmake -S . -B build
-sudo -u postgres cmake --build build
-cmake --install build --component extension # sudo
-sudo -u postgres cmake --build build --target installcheck
+cd build
 
+# first regular cmake
+sudo -u postgres cmake -S ..
+sudo -u postgres cmake --build .
+cmake --install . --component extension # sudo
+sudo -u postgres ctest --output-on-failure
+
+cd ..
 rm -rf build
 
 # also try makefile pgxn wrapper
-sudo -u postgres make all
-make install # sudo
-sudo -u postgres make installcheck
+#sudo -u postgres make all
+#make install # sudo
+#sudo -u postgres make test
--- cmake/FindPostgreSQL.cmake
+++ cmake/FindPostgreSQL.cmake
@@ -8,17 +8,46 @@ find_program(PostgreSQL_PG_CONFIG pg_config
   DOC "Path to the pg_config executable"
 )
 
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --includedir        OUTPUT_VARIABLE PostgreSQL_INCLUDE_DIR)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --includedir-server OUTPUT_VARIABLE PostgreSQL_INCLUDE_SERVER_DIR)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --libdir            OUTPUT_VARIABLE PostgreSQL_LIBRARY_DIR)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --pkglibdir         OUTPUT_VARIABLE PostgreSQL_PKG_LIBRARY_DIR)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --sharedir          OUTPUT_VARIABLE PostgreSQL_SHARE_DIR)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --cppflags          OUTPUT_VARIABLE PostgreSQL_CPP_FLAGS)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --cflags            OUTPUT_VARIABLE PostgreSQL_C_FLAGS)
-exec_program(${PostgreSQL_PG_CONFIG} ARGS --version           OUTPUT_VARIABLE PostgreSQL_VERSION_STRING)
+function(get_pg_config var)
+  execute_process(
+    COMMAND ${PostgreSQL_PG_CONFIG} ${ARGN}
+    OUTPUT_VARIABLE out
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+    message(STATUS "${var}: ${out}")
+  set(${var} ${out} PARENT_SCOPE)
+endfunction()
 
-set(PostgreSQL_INCLUDE_DIRS ${PostgreSQL_INCLUDE_DIR} ${PostgreSQL_INCLUDE_SERVER_DIR})
+get_pg_config(PostgreSQL_VERSION_STRING     --version)
+# linux: PostgreSQL 13.0 (Ubuntu 13.0-1.pgdg20.04+1)
+# win32: PostgreSQL 13.0
+get_pg_config(PostgreSQL_INCLUDE_DIR        --includedir)
+# linux: /usr/include/postgresql
+# win32: C:/PROGRA~1/POSTGR~1/13/include
+get_pg_config(PostgreSQL_INCLUDE_SERVER_DIR --includedir-server)
+# linux: /usr/include/postgresql/13/server
+# win32: C:/PROGRA~1/POSTGR~1/13/include/server
+get_pg_config(PostgreSQL_LIBRARY_DIR        --libdir)
+# linux: /usr/lib/x86_64-linux-gnu
+# win32: C:/PROGRA~1/POSTGR~1/13/lib
+get_pg_config(PostgreSQL_PKG_LIBRARY_DIR    --pkglibdir)
+# linux: /usr/lib/postgresql/13/lib
+# win32: C:/PROGRA~1/POSTGR~1/13/lib
+get_pg_config(PostgreSQL_SHARE_DIR          --sharedir)
+# linux: /usr/share/postgresql/13
+# win32: C:/PROGRA~1/POSTGR~1/13/share
+
+# below not set on WIN32:
+get_pg_config(PostgreSQL_CPP_FLAGS          --cppflags)
+get_pg_config(PostgreSQL_C_FLAGS            --cflags)
+get_pg_config(PostgreSQL_LD_FLAGS           --ldflags)
+get_pg_config(PostgreSQL_LIBS               --libs)
+
+
+# from timescaledb:
+# Only Windows and FreeBSD need the base include/ dir instead of include/server/
+set(PostgreSQL_INCLUDE_DIRS ${PostgreSQL_INCLUDE_SERVER_DIR})
 if(WIN32)
+  list(APPEND PostgreSQL_INCLUDE_DIRS ${PostgreSQL_INCLUDE_DIR})
   list(APPEND PostgreSQL_INCLUDE_DIRS ${PostgreSQL_INCLUDE_SERVER_DIR}/port/win32)
   if(MSVC)
     list(APPEND PostgreSQL_INCLUDE_DIRS ${PostgreSQL_INCLUDE_SERVER_DIR}/port/win32_msvc)
